024a3ed079bd34e13437dbde7ffc605b02473108
Hamidjon03 <forevalm@gmail.com>
2022-09-23 11:19:12 +0500

    crm model added

---
+++ models/crm.js
@@ -0,0 +1,36 @@
+const {Scheme, model} = require("mongoose");
+
+const crmScheme = new Scheme({
+  nameCenter: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+})
+
+module.exports = model("crmScheme", crmScheme)
