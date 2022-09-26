21817090de1b6a1618ecc8c34fc2281fa4be0d6d
Hamidjon03 <forevalm@gmail.com>
2022-09-26 10:53:15 +0500

    added teacher controller

--- controllers/teacher.controller.js
+++ controllers/teacher.controller.js
@@ -1,20 +1,36 @@
+const GeneretePassword = require('../helpers/generatePassword');
+const GenerateToken = require('../helpers/generateToken');
+const sendMessageEmail = require('../helpers/sendEmail');
+
 const SendMessage = require('../helpers/sendMessageUser');
 const TEACHER = require('./../models/teacher');
 
-exports.addTeacher = async(req,res,next) =>{
+exports.addTeacher = async (req, res, next) => {
     try {
-        const {} = req.body;       
+        const { science, phone, password, username, firstName, lastName, email } = req.body;
+        const condidate = await CRM.findOne({
+            $or:
+                [
+                    { firstName: firstName }, { lastName: lastName },
+                    { email: email }, { username: username },
+                    { password: password }, { phone: phone },
+                    { science: science }, { science: science },
+                    { science: science }
+                ]
+        });
+        if(condidate){
+            return SendMessage(res, 400, `Ooops, This Teacher already exist`)
+        }
+        const saltpass = await GeneretePassword.GeneretePassword(password);
     } catch (e) {
         SendMessage(res, 500, 'Internal Server Error.')
     }
 }
 
-
-
-// exports.addTeacher = async(req,res,next) =>{
-//     try {
-        
-//     } catch (e) {
-//         SendMessage(res, 500, 'Internal Server Error.')
-//     }
-// }
+exports.getTeachers = async(req, res, next) => {
+    try{    
+        res.send(`All teacher there`)
+    } catch (e) {
+        SendMessage(res, 500, 'Internal Server Error.')
+    }
+}
