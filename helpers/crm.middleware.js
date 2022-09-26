8ba5ffaaaf43cfaab241d44400e59b3ac18b677a
IxtiyorSadullayev <sadullayevixtiyor2200@gmail.com>
2022-09-26 10:53:04 +0500

    crn middleware

--- helpers/crm.middleware.js
+++ helpers/crm.middleware.js
@@ -1,3 +1,10 @@
 const jwt = require('jsonwebtoken');
-
-
+const SendMessage = require('./sendMessageUser');
+const CrmMiddleware = async (req,res,next) =>{
+    try {
+        const token = req.header.Authorization;
+        
+    } catch (e) {
+        SendMessage(res, 500, 'Internal Server Error');
+    }
+}
